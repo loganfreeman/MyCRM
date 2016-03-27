7d053e35c3e3c85cadd51d2f2dc83e31ee633c75
shanhong cheng <shnahongcheng@gmail.com>
2016-03-27 11:49:44 -0600

    delete test directory

--- test/controllers/accountant_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AccountantControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/factories/emp_data.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :emp_datum do
-    name "MyString"
-address "MyString"
-age 1
-  end
-
-end
