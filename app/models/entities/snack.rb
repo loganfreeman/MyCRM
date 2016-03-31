a9b7960bd454fa751f6e2b7f5de7a3155c50381a
shanhong cheng <shanhong.cheng@utah.edu>
2016-03-31 12:24:54 -0600

    all

--- app/models/entities/snack.rb
+++ app/models/entities/snack.rb
@@ -1,2 +1,9 @@
 class Snack < ActiveRecord::Base
+  belongs_to :itinerary_item
+  acts_as_taggable_on :tags
+  acts_as_commentable
+  has_paper_trail class_name: 'Version', ignore: [:subscribed_users]
+  has_fields
+  exportable
+  sortable by: ["created_at DESC", "updated_at DESC"], default: "created_at DESC"
 end
